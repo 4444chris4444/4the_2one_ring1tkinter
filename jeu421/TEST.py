ad9f7d76064dcb08d2e18536abe122ab3969ca2b
4444chris4444 <cgaudreau92@gmail.com>
2016-12-14 17:32:25 -0500

    test interface tkinter

--- jeu421/TEST.py
+++ jeu421/TEST.py
@@ -1,3 +1,13 @@
-import tkinter
+from tkinter import Tk, Label, Button
 
-#testing
+def interface():
+    root = Tk()
+    titre = Label(root, text="---- JEU 421 ----")
+    titre.grid(padx=250, pady=250)
+    bouton = Button(root, text="Continuer")
+    bouton.grid()
+
+    root.mainloop()
+
+
+interface()
